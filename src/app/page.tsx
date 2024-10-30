63bb35f273b98846008a2fee9fe1ce4f43e6d9e2
unknown <shaheryarshah2022@gmail.com>
2024-10-30 17:20:11 +0500

    Some message about the change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (<div className="flex flex-row gap-10 justify-center items-center min-h-screen">
     <div className="flexbox bg-red-500 h-80 w-80"></div>
-      <div className="flexbox bg-blue-500 h-80 w-80"></div>
       <div className="flexbox bg-green-500 h-80 w-80"></div>
+      <div className="flexbox bg-white h-80 w-80"></div>
       <div className="flexbox bg-white h-80 w-80"></div>  
     </div>
   );
